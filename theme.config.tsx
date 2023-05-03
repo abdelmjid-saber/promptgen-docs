5dc7057e7e4d87ee430c39953b45fa244ba098da
Abdelmjid Saber <87904671+abdelmjid-saber@users.noreply.github.com>
2023-05-03 14:20:34 +0300

    Add files via upload

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>PromptGen</span>,
+  project: {
+    link: 'https://github.com/abdelmjid-saber/promptgen-docs',
+  },
+  chat: {
+    link: '',
+  },
+  docsRepositoryBase: 'https://github.com/abdelmjid-saber/promptgen-docs/blob/main',
+  footer: {
+    text: 'PromptGen Docs',
+  },
+}
+
+export default config
